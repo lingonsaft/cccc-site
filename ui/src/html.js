017ad2973f89e9fc3bc6810007c92137721253b4
richie-south <richard.soderman@gmail.com>
2018-03-07 20:53:58 +0100

    fix: title

--- ui/src/html.js
+++ ui/src/html.js
@@ -36,7 +36,7 @@ class Html extends Component {
           />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="theme-color" content="#393F49" />
-          <title>CodeCardsCodingCards</title>
+          <title>CodeCardCodingCards</title>
 
           {css}
         </head>
